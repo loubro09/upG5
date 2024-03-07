0da370f11f61bbc0aa9b2be2d078276acd51987a
loubro09 <louis.r.brown@hotmail.com>
2024-03-07 16:29:18 +0100

    inloggning funktion

--- UP_G5/src/Entity/Message.java
+++ UP_G5/src/Entity/Message.java
@@ -1,8 +1,46 @@
 package Entity;
 
+import javax.swing.*;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 public class Message implements Serializable {
     private MessageType messageType;
+    private String text;
+    private Icon ImageIcon;
+    private User sender;
+    private List<User> receivers;
+    private Date timeDelivered;
+    private Date timeReceived;
 
+    public Message(String text, User sender, List<User> receivers, Date timeDelivered, Date timeReceived) {
+        this.text = text;
+        this.sender = sender;
+        this.receivers = receivers;
+        this.timeDelivered = timeDelivered;
+        this.timeReceived = timeReceived;
+    }
+
+    public String getText(){
+        return text;
+    }
+    public Icon getImageIcon(){
+        return ImageIcon;
+    }
+    public User getSender(){
+        return sender;
+    }
+    public List<User> getReceivers(){
+        return receivers;
+    }
+    public Date getTimeReceived(){
+        return timeReceived;
+    }
+    public void setTimeReceived(Date timeReceived) {
+        this.timeReceived = timeReceived;
+    }
+    public Date getTimeDelivered(){
+        return timeDelivered;
+    }
 }
