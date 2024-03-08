8790d74613d9061b83d038dbbbe247792f26f1f0
discodisco17 <nordenswanida@gmail.com>
2024-03-08 14:20:02 +0100

    gui register

--- UP_G5/src/Client/view/RegisterUserFrame.java
+++ UP_G5/src/Client/view/RegisterUserFrame.java
@@ -4,8 +4,8 @@ import Client.ClientViewController;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-        import java.awt.*;
-        import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
